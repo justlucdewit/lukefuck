40d19b1988797b0f9e2aad5b4c196ebd3ff1aba6
Luc de wit <luc.dewit@hotmail.nl>
2019-11-06 22:00:32 +0100

    added input/output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use std::io;
+//started on 6-11-2019
+
+use std::*;
+use std::io::Write;
 use std::env;
+use std::convert::TryInto;
 /*
 + increase current value    x
 - decrease current value    x
@@ -13,7 +17,7 @@ use std::env;
 ! debug                     V
 */
 
-fn input() -> std::string::String {
+fn input() -> String {
     let mut code = String::new();
 
     io::stdin().read_line(&mut code);
@@ -24,41 +28,65 @@ pub fn get_args() -> Vec<String> {
     return env::args().collect();
 }
 
+fn output(byte : u8){
+    print!("{}", byte as char);
+}
+
 fn interpreter(){
-    let values = vec![0];
-    let mut ptr = 0;
+    let mut values = vec![0];
+    let mut ptr : i128 = 0;
     loop{
         let code = input();
         for symbol in code.chars(){
             if symbol == '>'{
                 ptr+=1;
-                if true/*test if ptr index is in values*/{
-
+                if values.len()-1 < ptr as usize{
+                    values.push(0);
                 }
             }
 
             else if symbol == '<'{
                 ptr-=1;
-                if ptr < 0{
-                    ptr = 0;
+            }
+
+            else if symbol == '+'{
+                values[ptr as usize] += 1;
+            }
+
+            else if symbol == '-'{
+                values[ptr as usize] -= 1;
+            }
+
+            else if symbol == '.'{
+                output(values[ptr as usize]);
+            }
+
+            else if symbol == ','{
+                let tmp = input();
+                let mut str = tmp.trim();
+                if str.len() == 1{
+                    let byte = str.chars().next().unwrap() as u8;
+                    values[ptr as usize] = byte;
+                }else{
+                    println!("[LF ERROR 001] CANT INPUT MORE THEN 1 CHARACTER");
                 }
             }
 
             else if symbol == '!'{
                 println!("pointer position: {}", ptr);
-                let v = "";
                 for (i, v) in values.iter().enumerate(){
                     println!("value {}: {}", i, v)
                 }
-                print!("\n");
             }
         }
+        io::stdout().flush();
+        println!("\n");
     }
 }
 
 fn main(){
     let args = get_args();
-    const VERSION : &str = "Lukefuck 0.0.0a";
+    const VERSION : &str = "Lukefuck 0.0.0b";
     println!("{}", VERSION);
 
     if args[1] == "playground"{
