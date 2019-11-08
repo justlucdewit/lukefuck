2e81afc7d468aa808b617c91d354552ff83525f8
Luc de wit <luc.dewit@hotmail.nl>
2019-11-08 16:59:11 +0100

    added more custom commands

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,32 @@ use std::*;
 use std::io::Write;
 use std::env;
 use std::convert::TryInto;
+
+use rand::Rng;
 /*
-+ increase current value    V
-- decrease current value    V
++ increase current value    V M
+- decrease current value    V M
 > increase pointer pos      V
 < decrease pointer pos      V
-[ loop open                 x
-] loop close                x
+[ loop open                 V
+] loop close                V
 . output current value      V
 , input into current value  V
 
 ! debug                     V
+@ reset pointer             V
+# reset everything          V EM
+; reset current value       V M
+
+/ mark current cell as locked   V
+| mark current cell as open     V
+
+? fill current cell with random value between 0 and 255     X
+~ fill current cell with random boolean                     X
+
 */
+
+//-------------------------------------------helper functions---------------------
 
 fn input() -> String {
     let mut code = String::new();
@@ -32,12 +46,19 @@ fn output(byte : u8){
     print!("{}", byte as char);
 }
 
-fn interpreter(){
+//------------------------------------main program---------------------------------
+
+fn interpreter(filecode:&str){
     let mut values = vec![0 as i128];
     let mut ptr : i128 = 0;
     let mut loop_mem = vec![];
+    let mut locked_cells = vec![];
     loop{
-        let code = input();
+        let mut code = filecode.to_string();
+        if filecode == "pg"{
+            code = input();
+        }
+
         let mut reading_index : i128 = -1;
         loop{
             reading_index += 1;
@@ -46,7 +67,6 @@ fn interpreter(){
             }
 
             let symbol = code.chars().nth(reading_index as usize).unwrap();
-
             if symbol == '>'{
                 ptr+=1;
                 if values.len()-1 < ptr as usize{
@@ -59,18 +79,26 @@ fn interpreter(){
             }
 
             else if symbol == '+'{
-                if values[ptr as usize] == 255{
-                    values[ptr as usize] = 0;
+                if locked_cells.contains(&ptr) == false{
+                    if values[ptr as usize] == 255{
+                        values[ptr as usize] = 0;
+                    }else{
+                        values[ptr as usize] += 1;
+                    }
                 }else{
-                    values[ptr as usize] += 1;
+                    println!("[LF ERROR 102] CANT CHANGE VALUE OF CELL {} BECAUSE IT IS LOCKED WITH VALUE {}", ptr, values[ptr as usize]);
                 }
             }
 
             else if symbol == '-'{
-                if values[ptr as usize] == 0{
-                    values[ptr as usize] = 255;
+                if locked_cells.contains(&ptr) == false{
+                    if values[ptr as usize] == 0{
+                        values[ptr as usize] = 255;
+                    }else{
+                        values[ptr as usize] -= 1;
+                    }
                 }else{
-                    values[ptr as usize] -= 1;
+                    println!("[LF ERROR 102] CANT CHANGE VALUE OF CELL {} BECAUSE IT IS LOCKED WITH VALUE {}", ptr, values[ptr as usize]);
                 }
             }
 
@@ -112,18 +140,101 @@ fn interpreter(){
                     println!("loop mem {}: {}", i, v)
                 }
             }
+
+            else if symbol == '@'{
+                ptr = 0;
+            }
+
+            else if symbol == '#'{
+                if locked_cells.len() == 0{
+                    ptr = 0;
+                    values = vec![0];
+                }else{
+                    println!("[LF ERROR 102] CANT CHANGE VALUE OF CELL {} BECAUSE IT IS LOCKED WITH VALUE {}", ptr, values[ptr as usize]);
+                }
+            }
+
+            else if symbol == ';'{
+                if locked_cells.contains(&ptr) == false{
+                    values[ptr as usize] = 0;
+                }else{
+                    println!("[LF ERROR 102] CANT CHANGE VALUE OF CELL {} BECAUSE IT IS LOCKED WITH VALUE {}", ptr, values[ptr as usize]);
+                }
+            }
+
+            else if symbol == '/'{
+                if locked_cells.contains(&ptr){
+                    println!("[ERROR 103] CANT LOCK CELL BECAUSE A CELL IS STILL LOCKED");
+                }else{
+                    locked_cells.push(ptr);
+                }
+            }
+
+            else if symbol == '|'{
+                if locked_cells.contains(&ptr){
+                    //delete value from vector
+                    let mut index_of_locked = 69420;
+                    for (i, cell) in locked_cells.iter().enumerate(){
+                        if cell == &ptr{
+                            index_of_locked = i;
+                            break;
+                        }
+                    }
+                    println!("removing index {}", index_of_locked);
+                    locked_cells.remove(index_of_locked);
+                }else{
+                    println!("[ERROR 103] CANT UNLOCK CELL BECAUSE CELL {} IS ALREADY UNLOCKED", ptr);
+                }
+            }
+
+            else if symbol == '?'{
+                if locked_cells.contains(&ptr) == false{
+                    let r : u8 = rand::thread_rng().gen_range(0, 255);
+                    values[ptr as usize] = r.try_into().unwrap();
+                }else{
+                    println!("[LF ERROR 102] CANT CHANGE VALUE OF CELL {} BECAUSE IT IS LOCKED WITH VALUE {}", ptr, values[ptr as usize]);
+                }
+            }
+
+            else if symbol == '~'{
+                if locked_cells.contains(&ptr) == false{
+                    let r : u8 = rand::thread_rng().gen_range(0, 2);
+                    values[ptr as usize] = r.try_into().unwrap();
+                }else{
+                    println!("[LF ERROR 102] CANT CHANGE VALUE OF CELL {} BECAUSE IT IS LOCKED WITH VALUE {}", ptr, values[ptr as usize]);
+                }
+            }
         }
         io::stdout().flush();
         println!("\n");
+
+        if filecode != "pg"{
+            break;
+        }
     }
 }
 
+
 fn main(){
     let args = get_args();
-    const VERSION : &str = "Lukefuck 0.0.1b";
+    const VERSION : &str = "Lukefuck 0.0.2a";
     println!("{}", VERSION);
 
     if args[1] == "playground"{
-        interpreter();
+        interpreter("pg");
     }
+
+    else if args[1]== "run"{
+        let path = &args[2];
+        let code = read_file(path);
+        interpreter(&code);
+    }
+    
+    else{
+        println!("command not understood, try 'Lukefuck playground'");
+    }
+}
+
+pub fn read_file(filename: &str) -> String {
+    return fs::read_to_string(filename).expect("ERROR WHILE LOADING CANT FIND FILE");
 }
